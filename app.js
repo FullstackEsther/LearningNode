d6a8165956042457293094239e50e0411762b1d2
Esther <otufaleesther@gmail.com>
2024-12-23 13:31:12 +0100

    second commit

--- app.js
+++ app.js
@@ -37,6 +37,24 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 });
 
+app.put('/api/courses/:id',(req,res)=> {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send("The course with the given ID is not found");
+    const {error} = validate (req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    course.name= req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id',(req,res)=> {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send("The course with the given ID is not found");
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    res.send(course);
+})
+
+
 
 function validate(course){
     const schema = Joi.object({
